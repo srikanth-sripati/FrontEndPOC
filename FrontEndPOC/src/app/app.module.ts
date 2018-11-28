20fa6a86f989f67464d66ccb1da0a667e1b781e0
srikanth-sripati <srikanth.sripati8195@gmail.com>
2018-11-28 23:19:07 +0530

    Creation of components and service files

--- FrontEndPOC/src/app/app.module.ts
+++ FrontEndPOC/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentTableComponent } from './student-table/student-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentFormComponent,
+    StudentTableComponent
   ],
   imports: [
     BrowserModule
